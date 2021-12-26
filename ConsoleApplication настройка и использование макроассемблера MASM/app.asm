ae7847b61cc54b30eb7717e401f71e4ee885a5a2
Anna <annikamintel99@yandex.ru>
2021-12-27 00:17:32 +0300

    init: created lab (working with MAMS.Basic lavel)

---
+++ ConsoleApplication настройка и использование макроассемблера MASM/app.asm
@@ -0,0 +1,22 @@
+.686
+.model flat, stdcall
+include macroses.inc
+
+.data
+include vars.inc
+
+.code
+include procedures.inc
+
+WinMain PROC
+
+    INIT_CONSOLE
+
+    WRITELN 'Hello, world!'
+    WRITELN 'Çהנאגסעגףי, לטנ! ÌÅÍ‗ ÇÎÂÓÒ ÀÍÍÀ'
+
+    READLN_CONSOLE
+    INVOKE ExitProcess, 0
+
+WinMain ENDP
+end WinMain
